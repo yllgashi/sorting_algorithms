a28f98fc5b81c371e073675f791b0d926eb4f91a
yllgasshi <yllgashi282@gmail.com>
2020-03-30 03:25:34 +0200

    CreateArrayMethod Fix

--- Sorting_Algorithms/SortNumbers.cs
+++ Sorting_Algorithms/SortNumbers.cs
@@ -1,29 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sorting_Algorithms
 {
     class SortNumbers
     {
+        /// <summary>
+        /// Set size of array
+        /// </summary>
         public void CreateArray()
-        {
-            Console.Write("Size of your array: ");
-            int arraySize = int.Parse(Console.ReadLine());
+        {   
+            bool isNumber = false;
+            int arraySize = 0;
+
+            while (!isNumber)
+            {
+                Console.Write("Size of your array: ");
+                isNumber = int.TryParse(Console.ReadLine(), out arraySize);
+            }
+            
+
+
             int[] array = new int[arraySize];
+            Random r = new Random();
 
             for (int i = 0; i < array.Length; i++)
             {
-                Console.Write($"Element {i + 1}: ");
-                array[i] = int.Parse(Console.ReadLine());
+                array[i] = r.Next(0,1000);
             }
 
-            Choose(array);
+            ChooseSortAlgorithm(array);
         }
 
-        public void Choose(int[] array)
+        /// <summary>
+        /// Choose sort alg
+        /// </summary>
+        public void ChooseSortAlgorithm(int[] array)
         {
             while (true)
             {
@@ -35,12 +46,19 @@ namespace Sorting_Algorithms
                 Console.WriteLine("4. Insertion Sort");
                 Console.WriteLine("5. Insertion Sort (combination of sequential and binary search)");
                 Console.WriteLine("6. Quit");
-                Console.Write("\n Enter your number: ");
                 Console.ForegroundColor = ConsoleColor.White;
-                int answer = int.Parse(Console.ReadLine());
+
+                bool isNumber = false;
+                int answer = 0;
+                while (!isNumber)
+                {
+                    Console.Write("\n Enter your number: ");
+                    isNumber = int.TryParse(Console.ReadLine(), out answer);
+                }
 
                 if (answer == 1)
                 {
+                    Console.Clear();
                     ShowArray(array);
                 }
                 else if (answer == 2)
@@ -69,7 +87,6 @@ namespace Sorting_Algorithms
                 }
             }
         }
-
         public void BubbleSort(int[] array)
         {
             Console.Clear();
@@ -108,7 +125,6 @@ namespace Sorting_Algorithms
                 }
             }
         }
-        /////// Shto edhe mundesine kombinim binar me sekuencial
 
         public void InsertionSort(int[] array)
         {
@@ -141,7 +157,10 @@ namespace Sorting_Algorithms
                 }
             }
         }
-
+        /// <summary>
+        /// Insertion Sort where element find his place with sequential search
+        /// </summary>
+        /// <param name="array">Array to sort</param>
         public void SequentialInsert(int[] array)
         {
             Console.Clear();
@@ -158,47 +177,10 @@ namespace Sorting_Algorithms
                 }
             }
         }
-
-        public void SequentialAndBinaryInsert(int[] array)
-        {
-            Console.Clear();
-            for (int i = 1; i < array.Length; i++)
-            {
-                while (true)
-                {
-                    int middle = i / 2;
-                    if (array[i] < array[middle])
-                    {
-                        for (int j = i; j > 0; j--)
-                        {
-                            if (array[j] < array[j - 1])
-                            {
-                                int temp = array[j - 1];
-                                array[j - 1] = array[j];
-                                array[j] = temp;
-                            }
-                        }
-                    }
-                    else if (array[i] <= array[i - 1])
-                    {
-                        for (int j = i; j >= middle; j--)
-                        {
-                            if (array[j] < array[j - 1])
-                            {
-                                int temp = array[j - 1];
-                                array[j - 1] = array[j];
-                                array[j] = temp;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
+        /// <summary>
+        /// Insertion Sort where element find his place with binary search
+        /// </summary>
+        /// <param name="array">Array to sort</param>
         public void BinarySearchSort(int[] array)
         {
             Console.Clear();
@@ -261,6 +243,46 @@ namespace Sorting_Algorithms
             }
         }
 
+        public void SequentialAndBinaryInsert(int[] array)
+        {
+            Console.Clear();
+            for (int i = 1; i < array.Length; i++)
+            {
+                while (true)
+                {
+                    int middle = i / 2;
+                    if (array[i] < array[middle])
+                    {
+                        for (int j = i; j > 0; j--)
+                        {
+                            if (array[j] < array[j - 1])
+                            {
+                                int temp = array[j - 1];
+                                array[j - 1] = array[j];
+                                array[j] = temp;
+                            }
+                        }
+                    }
+                    else if (array[i] <= array[i - 1])
+                    {
+                        for (int j = i; j >= middle; j--)
+                        {
+                            if (array[j] < array[j - 1])
+                            {
+                                int temp = array[j - 1];
+                                array[j - 1] = array[j];
+                                array[j] = temp;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
         public void MoveForwardElementOfArray(int[] array, int middle, int sortingElement)
         {
             for (int i = sortingElement; i > middle; i--)
@@ -273,7 +295,7 @@ namespace Sorting_Algorithms
 
         public void ShowArray(int[] array)
         {
-            Console.WriteLine("#########################");
+            Console.WriteLine();
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(array[i] + " ");
